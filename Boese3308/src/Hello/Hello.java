92ab69cde344eb8216a32c204ae670d4907f42b7
tydr <tyler.dresser@colorado.edu>
2014-09-17 12:01:58 -0600

    troisième

--- Boese3308/src/Hello/Hello.java
+++ Boese3308/src/Hello/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args){
 		System.out.println("Hello World!");
 		System.out.println("How are you?"); //Line added in Git again
+		System.out.println("Milkshakes...");
 	}
 
 }
